f9a0ef6cf8859501867d4d347c10715c04f29941
Jhones S. Clementino <70949925+jhones@users.noreply.github.com>
2021-05-25 13:20:51 -0300

    Add Textures image drawing example (#20)
    * Remove unused variables from example files (#18)
    
    * Add textures_image_drawing example
    
    * Adjustments for update image struct object
    
    * Adjustments for update image struct object
    
    * Add textures_image_drawing example
    
    * Review adjustments made

---
+++ src/Types/Font.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nawarian\Raylib\Types;
+
+use FFI;
+use FFI\CData;
+use InvalidArgumentException;
+use Nawarian\Raylib\RaylibFFIProxy;
+
+class Font
+{
+    public int $baseSize;
+    public int $charsCount;
+    public int $charsPadding;
+    public CData $texture;
+    public CData $recs;
+    public CData $chars;
+
+    public function __construct(
+        int $baseSize,
+        int $charsCount,
+        int $charsPadding,
+        CData $texture,
+        CData $recs,
+        CData $chars
+    ) {
+        $this->baseSize = $baseSize;
+        $this->charsCount = $charsCount;
+        $this->charsPadding = $charsPadding;
+        $this->texture = $texture;
+        $this->recs = $recs;
+        $this->chars = $chars;
+    }
+
+    /**
+     * @psalm-suppress MixedPropertyAssignment
+     * @psalm-suppress UndefinedPropertyAssignment
+     */
+    public function toCData(RaylibFFIProxy $ffi): CData
+    {
+        try {
+            $font = $ffi->new('Font');
+        } catch (FFI\ParserException $e) {
+            throw new InvalidArgumentException(
+                'Object $ffi does not provide the type "struct Font"'
+            );
+        }
+
+        $font->baseSize = $this->baseSize;
+        $font->charsCount = $this->charsCount;
+        $font->charsPadding = $this->charsPadding;
+        $font->texture = $this->texture;
+        $font->recs = $this->recs;
+        $font->chars = $this->chars;
+
+        return $font;
+    }
+}
