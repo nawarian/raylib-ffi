da96562bc9f2bcaad4b8dfb46cdb913ab8f53488
Jhones dos Santos Clementino <jhones.developer@gmail.com>
2021-06-14 10:00:53 -0300

    Add textures_mouse_painting example

--- examples/textures/textures_mouse_painting.php
+++ examples/textures/textures_mouse_painting.php
@@ -20,14 +20,14 @@ $screenHeight = 450;
 $raylib->initWindow($screenWidth, $screenHeight, 'raylib [textures] example - mouse painting');
 
 // Colours to choose from
-$colors = [
-    Color::rayWhite(), Color::yellow(), Color::gold(), Color::orange(),
-    Color::pink(), Color::red(), Color::maroon(), Color::green(),
-    Color::lime(), Color::darkGreen(), Color::skyBlue(), Color::blue(),
-    Color::darkBlue(), Color::purple(), Color::violet(), Color::darkPurple(),
-    Color::beige(), Color::brown(), Color::darkBrown(), Color::lightGray(),
-    Color::gray(), Color::darkGray(), Color::black()
-];
+//$colors = [
+//    Color::rayWhite(), Color::yellow(), Color::gold(), Color::orange(),
+//    Color::pink(), Color::red(), Color::maroon(), Color::green(),
+//    Color::lime(), Color::darkGreen(), Color::skyBlue(), Color::blue(),
+//    Color::darkBlue(), Color::purple(), Color::violet(), Color::darkPurple(),
+//    Color::beige(), Color::brown(), Color::darkBrown(), Color::lightGray(),
+//    Color::gray(), Color::darkGray(), Color::black()
+//];
 
 // Define colorsRecs data (for every rectangle)
-$colorRecs = [];
+//$colorRecs = [];
